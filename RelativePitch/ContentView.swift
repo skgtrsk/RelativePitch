16bb8912d3be69a615be65fb76fc3d627a298c43
Ryo Sakaguchi <Ryo Sakaguchi>
2022-10-14 20:17:07 +0900

    Initial Commit

---
+++ RelativePitch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RelativePitch
+//
+//  Created by 坂口諒 on 2022/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RelativePitch/RelativePitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelativePitchApp.swift
+//  RelativePitch
+//
+//  Created by 坂口諒 on 2022/10/14.
+//
+
+import SwiftUI
+
+@main
+struct RelativePitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
